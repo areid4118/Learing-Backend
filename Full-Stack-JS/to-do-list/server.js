37c675a82345f570c0358ad72800633481087efd
Alex Reid <areid4118@gmail.com>
2020-06-09 03:39:04 -0400

    added corret code for create item dynamically

--- Full-Stack-JS/to-do-list/server.js
+++ Full-Stack-JS/to-do-list/server.js
@@ -23,68 +23,54 @@ app.get('/', function (req, res) {
 	db.collection('items')
 		.find()
 		.toArray(function (err, items) {
-			res.send(`
-		<!DOCTYPE html>
-			<html>
-				<head>
-					<meta charset="UTF-8" />
-					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					<title>Simple To-Do App</title>
-					<link
-						rel="stylesheet"
-						href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-						integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-						crossorigin="anonymous"
-					/>
-					<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js" defer></script>
-					<script src="/browser.js" defer></script>
-				</head>
-				<body>
-					<div class="container">
-						<h1 class="display-4 text-center py-1">To-Do App</h1>
-			
-						<div class="jumbotron p-3 shadow-sm">
-							<form id="create-form" action="/create-item" method="POST">
-								<div class="d-flex align-items-center">
-									<input
-										id="create-field"
-										name="item"
-										autofocus
-										autocomplete="off"
-										class="form-control mr-3"
-										type="text"
-										style="flex: 1;"
-									/>
-									<button class="btn btn-primary">Add New Item</button>
-								</div>
-							</form>
-						</div>
-			
-						<ul id="item-list" class="list-group pb-5">
-							${items
-								.map((item) => {
-									return `<li
-								class="list-group-item list-group-item-action d-flex align-items-center justify-content-between"
-							>
-								<span class="item-text">${item.text}</span>
-								<div>
-									<button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-									<button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-								</div>
-							</li>`;
-								})
-								.join('')}
-						</ul>
-					</div>
-				</body>
-			</html>
-		`);
+			res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple To-Do App</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <h1 class="display-4 text-center py-1">To-Do App!</h1>
+  
+  <div class="jumbotron p-3 shadow-sm">
+  <form id="create-form" action="/create-item" method="POST">
+  <div class="d-flex align-items-center">
+  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+  <button class="btn btn-primary">Add New Item</button>
+  </div>
+  </form>
+  </div>
+  
+  <ul id="item-list" class="list-group pb-5">
+    ${items
+			.map(function (item) {
+				return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+      <span class="item-text">${item.text}</span>
+      <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+      </div>
+      </li>`;
+			})
+			.join('')}
+  </ul>
+  
+  </div>
+  
+	
+
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+  </body>
+  </html>`);
 		});
 });
 
 app.post('/create-item', function (req, res) {
 	db.collection('items').insertOne({ text: req.body.text }, function (err, info) {
-		// this is how you give the front end access to the mongodb id
 		res.json(info.ops[0]);
 	});
 });
@@ -99,12 +85,8 @@ app.post('/update-item', function (req, res) {
 	);
 });
 
-app.post('/delete-item', (req, res) => {
-	// method to delte is called delete one
-	// first argument is the docuement you want to delte
-	// second arguement is the function that is going to run once the action is complete
-
-	db.collection('items').deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, () => {
-		res.send('Sucess');
+app.post('/delete-item', function (req, res) {
+	db.collection('items').deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, function () {
+		res.send('Success');
 	});
 });
